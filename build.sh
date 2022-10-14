db31b4436bc32b0d0870ab33c43f393bbe25de1c
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2022-10-14 13:19:57 -0400

    .......

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ rm -Rf /usr/local/go && mkdir "$GO_DIR"
 curl --fail --show-error --silent --location "https://go.dev/dl/go1.19.2.darwin-amd64.tar.gz" -o "go1.19.2.darwin-amd64.tar.gz"
 tar xz --directory="$GO_DIR" --strip-components=1
 export PATH="$GO_DIR/bin":"$PATH"
+echo $PATH
 go version
 go env
 
