bc1f1bb523ee42ad7f436109e68b922c785e2696
ericzzzzzzz <102683393+ericzzzzzzz@users.noreply.github.com>
2023-11-20 17:27:00 -0500

    fix: tes

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -17,13 +17,25 @@ set -e
 # in the job configuration.
 cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-ericwork"
 
-echo "from continuous" > a.txt
-echo "from continuous" > c.txt
-mkdir out
-mv *.txt out
-mkdir -p "${KOKORO_ARTIFACTS_DIR}"/build/unsigned
-cp -R out/. "${KOKORO_ARTIFACTS_DIR}"/build/unsigned
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-git clone https://github.com/GoogleContainerTools/skaffold.git
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker version
+
+clone https://github.com/GoogleContainerTools/skaffold.git
 cd skaffold
-make coverage
